23c9fd969e03a92656d33daf96fe236991dec4a3
puelwon <ghdusdp1@gmail.com>
2022-06-22 14:45:54 +0900

    2차시

---
+++ pystudy_2/code_study.py
@@ -0,0 +1,14 @@
+
+score = int(input("스코어의 값은?"))
+scor = int(input("스코어 1의 값은?"))
+
+if score > 1 and scor <= 1 or score == 100:
+    print("yes")
+elif scor < 2:
+    print("ye")
+else:
+    print("no")
+true=1
+if true:
+    print("1")
+
