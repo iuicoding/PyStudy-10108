d5ebcfd9dc7f56a925fde74f100728b06ccfc5c5
puelwon <ghdusdp1@gmail.com>
2022-08-24 15:12:47 +0900

    3차시

---
+++ pyStudy3/function.py
@@ -0,0 +1,16 @@
+def printHelloWorld():
+    print("Hello World!!")
+    return
+
+def printAdd(a, b):
+    print(a+b)
+    return
+    print("응 없어")
+
+
+def add(a,b):
+    return a+b
+
+printHelloWorld()
+printAdd(1, 2)
+print(add(1, 3))
