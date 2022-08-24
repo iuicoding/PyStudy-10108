22e06c508984229568449a5a42006721f21434b3
puelwon <ghdusdp1@gmail.com>
2022-08-24 15:28:14 +0900

    3차시

--- pyStudy3/function.py
+++ pyStudy3/function.py
@@ -1,18 +1,26 @@
+
 def printHelloWorld(n=3):
-    print("Hello World!!"*n)
+    print("Hello World!!" * n)
     return
 
+
 def printAdd(a, b):
-    print(a+b)
+    print(a + b)
     return
     print("응 없어")
 
 
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
+
+
+def printWithComma(*b):
+    print(b)
+
 
 printHelloWorld()
 printHelloWorld(4)
 printHelloWorld(n=5)
 printAdd(1, 2)
 print(add(1, 3))
+printWithComma(1,2,3,4,2,4)
