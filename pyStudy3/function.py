8e8ec32dd65073bd01152adef831a5adcf6a5493
puelwon <ghdusdp1@gmail.com>
2022-08-24 15:45:05 +0900

    3차시

--- pyStudy3/function.py
+++ pyStudy3/function.py
@@ -1,4 +1,3 @@
-
 def printHelloWorld(n=3):
     print("Hello World!!" * n)
     return
@@ -15,9 +14,9 @@ def add(a, b):
 
 
 def addlist(*b):
-    sum=0
+    sum = 0
     for b in b:
-        sum+=b
+        sum += b
     return sum
 
 
@@ -26,4 +25,4 @@ printHelloWorld(4)
 printHelloWorld(n=5)
 printAdd(1, 2)
 print(add(1, 3))
-print(addlist(1,2,3,2,4,5,2,6))
+print(addlist(1, 2, 3, 2, 4, 5, 2, 6))
